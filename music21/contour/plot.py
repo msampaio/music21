4d4f579fcf024f32d9b3e27c2d5f4ba7d806bea9
Marcos da Silva Sampaio <marcos@sampaio.me>
2012-02-17 14:58:05 -0200

    Insert plot module for contour plotting

---
+++ music21/contour/plot.py
@@ -0,0 +1,59 @@
+# import unittest, doctest
+# import random, math, sys, os
+
+import music21
+from music21.graph import Graph
+from music21.graph import getColor
+
+try:
+    import matplotlib
+    import matplotlib.pyplot as plt
+
+except ImportError:
+    _missingImport.append('matplotlib')
+
+
+class GraphPlot(Graph):
+    '''Graph the count of a single element.
+
+    Data set is simply an ordered list of y elements.
+
+    >>> import music21.contour.plot
+    >>> data = [3, 4, 8, 2, 9]
+    >>> g = music21.contour.plot.GraphPlot(doneAction=None)
+    >>> g.setData(data)
+    >>> g.process()
+
+    .. image:: images/GraphPlot.*
+        :width: 600
+
+    '''
+
+    def __init__(self, *args, **keywords):
+
+        Graph.__init__(self, *args, **keywords)
+        self.axisKeys = ['x', 'y']
+        self._axisInit()
+
+    def process(self):
+        # figure size can be set w/ figsize=(5,10)
+        self.fig = plt.figure()
+        self.fig.subplots_adjust(left=0.15)
+
+        self.setAxisLabel('y', 'C-point value')
+        self.setAxisLabel('x', 'C-point position')
+
+        ax = self.fig.add_subplot(1, 1, 1)
+
+        x = range(len(self.data))
+        y = self.data
+
+        self.setTicks('x', [[a, str(a)] for a in x])
+        self.setTicks('y', [[b, str(b)] for b in range(min(y), max(y) + 1)])
+
+        ax.plot(x, y, 'o-', color=getColor(self.colors[0]), linewidth=1.8, label=str(self.data))
+        ax.legend(prop=matplotlib.font_manager.FontProperties(size=10))
+
+        self._adjustAxisSpines(ax)
+        self._applyFormatting(ax)
+        self.done()
